d045e77b9eeb65766c50373d0ccf60d6f8d3e1b0
David Mihola <david.mihola@sperling.cz>
2022-11-10 15:33:18 +0100

    yolo_models-data sets merge

--- data_preprocessing/character_plot.py
+++ data_preprocessing/character_plot.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import random as rnd
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from const_config import TRAINING_NPY_IMAGE_PATH
+from const_config import TRAINING_PREPROCESSED_PATH
 PLOT_WIDTH_HEIGHT = 4
 
-for file_name in os.listdir(f"{TRAINING_NPY_IMAGE_PATH}"):
-    samples = np.load(f"{TRAINING_NPY_IMAGE_PATH}{file_name}", allow_pickle=True)
+for file_name in os.listdir(f"{TRAINING_PREPROCESSED_PATH}"):
+    samples = np.load(f"{TRAINING_PREPROCESSED_PATH}{file_name}", allow_pickle=True)
     sample_count = samples.shape[0]
     _, axes = plt.subplots(PLOT_WIDTH_HEIGHT, PLOT_WIDTH_HEIGHT, figsize=(14, 14))
     for i in range(PLOT_WIDTH_HEIGHT):
---
+++ data_preprocessing/datasets_merge.py
@@ -0,0 +1,61 @@
+from PIL import Image
+import os
+import sys
+import numpy as np
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from const_config import DIGIT_AND_OPERATORS_1_PATH
+from const_config import DIGIT_AND_OPERATORS_2_PATH
+from const_config import ALL_MERGED_PREPROCESSED_PATH
+from const_config import IMAGE_WIDTH
+from const_config import IMAGE_HEIGHT
+from const_config import ALL_IMAGES_FILENAME
+from const_config import ALL_LABELS_FILENAME
+
+label_dict = { "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, 
+               "+": 10, "-": 11, "*": 12, "%": 13 }
+
+data_set_1 = np.concatenate((np.load(f"{DIGIT_AND_OPERATORS_1_PATH}CompleteDataSet_training_tuples.npy", allow_pickle=True),
+                             np.load(f"{DIGIT_AND_OPERATORS_1_PATH}CompleteDataSet_testing_tuples.npy", allow_pickle=True),
+                             np.load(f"{DIGIT_AND_OPERATORS_1_PATH}CompleteDataSet_validation_tuples.npy", allow_pickle=True)))
+                            
+data_set_1 = data_set_1[((data_set_1[:, 1] >= "0") & (data_set_1[:, 1] <= "9")) | (data_set_1[:, 1] == "+") |
+                         (data_set_1[:, 1] == "-") | (data_set_1[:, 1] == "*") | (data_set_1[:, 1] == "%")]
+
+sample_count = data_set_1.shape[0]
+for directory in os.listdir(f"{DIGIT_AND_OPERATORS_2_PATH}"):
+    sample_count += len(os.listdir(f"{DIGIT_AND_OPERATORS_2_PATH}{directory}"))
+
+images = np.zeros((sample_count, IMAGE_HEIGHT, IMAGE_WIDTH), dtype=np.float32)
+labels = np.zeros(sample_count, dtype=np.uint8)
+
+indices = np.random.randint(0, sample_count, sample_count) # randomly place images and labels in the final file
+index = 0
+
+for image_label in data_set_1:
+    images[indices[index]] = image_label[0]
+    labels[indices[index]] = label_dict[image_label[1]]
+    index += 1
+
+for directory, label in [("0/", 0), ("1/", 1), ("2/", 2), ("3/", 3), ("4/", 4), ("5/", 5), ("6/", 6),
+                                    ("7/", 7), ("8/", 8), ("9/", 9),
+                                    ("plus/", 10), ("minus/", 11), ("asterisk/", 12), ("slash/", 13)]:
+    file_names = os.listdir(f"{DIGIT_AND_OPERATORS_2_PATH}{directory}")
+    for file_name in os.listdir(f"{DIGIT_AND_OPERATORS_2_PATH}{directory}"):
+        image = np.array(Image.open(f"{DIGIT_AND_OPERATORS_2_PATH}{directory}{file_name}").resize((IMAGE_HEIGHT, IMAGE_WIDTH)))
+
+        if label == 12: # asterisks are from a different data set and are already white on black
+            image = np.array(image > 64, dtype=np.float32) # treshold
+        else:
+            image = np.array(image < 192, dtype=np.float32) # treshold
+        
+        images[indices[index], :, :] = image
+        labels[indices[index]] = label
+        index += 1
+    
+np.save(f"{ALL_MERGED_PREPROCESSED_PATH}{ALL_IMAGES_FILENAME}", images)
+np.save(f"{ALL_MERGED_PREPROCESSED_PATH}{ALL_LABELS_FILENAME}", labels)
+
+print(f"Number of samples: {sample_count}")
+print(f"Images file size: {os.stat(f'{ALL_MERGED_PREPROCESSED_PATH}{ALL_IMAGES_FILENAME}').st_size / (1024 * 1024)} MB")
+print(f"Labels file size: {os.stat(f'{ALL_MERGED_PREPROCESSED_PATH}{ALL_LABELS_FILENAME}').st_size / (1024 * 1024)} MB")

bd98d9a51a711d766c410aa7c9686cc364e72435
Robert Stojanovic <rstojanovic98@gmail.com>
2022-11-17 12:40:52 +0100

    Changes cropping method to keep correct aspect ratio

--- annotation/designer.py
+++ annotation/designer.py
@@ -53,7 +53,7 @@ class Designer:
             interpolation = np.random.choice(Designer.available_interpolation)
             self.interpolation = eval(f'cv2.{interpolation}')
 
-    def check_and_expand_region(self, pt):
+    def check_and_expand_draw_region(self, pt):
         padding = 2
         min_point_x = max(pt[0] - (self.thickness / 2) - padding, 0)
         min_point_y = max(pt[1] - (self.thickness / 2) - padding, 0)
@@ -73,6 +73,28 @@ class Designer:
         if max_point_y > self.region_max[1]:
             self.region_max[1] = max_point_y
 
+    def get_draw_region_img(self):
+        if self.region_min is None or self.region_max is None:
+            return self.img
+        width = self.region_max[0] - self.region_min[0]
+        height = self.region_max[1] - self.region_min[1]
+        if width == 0 or height == 0:
+            return self.img
+        if width > height:
+            center_height = (self.region_max[1] + self.region_min[1]) / 2.0
+            height = width
+            y = int(max(center_height - height / 2, 0))
+            x = self.region_min[0]
+        elif height > width:
+            center_width = (self.region_max[0] + self.region_min[0]) / 2.0
+            width = height
+            x = int(max(center_width - width / 2, 0))
+            y = self.region_min[1]
+        else:
+            x = self.region_min[0]
+            y = self.region_min[1]
+        return self.img[y:y+height, x:x+width]
+
     def draw(self, pt_1, pt_2):
         value = np.random.randint(*self.range_value) # random pixel value
         line_img = self.img * 0 # create an image to draw the new linea
@@ -91,16 +113,8 @@ class Designer:
         self.img = self.img + (255 - self.img) / 255 * line_img
         # Crop the output image to only the drawn region
         if self.crop_to_draw_region:
-            self.check_and_expand_region(pt_2)
-            if self.region_min is not None and self.region_max is not None:
-                x = self.region_min[0]
-                y = self.region_min[1]
-                width = self.region_max[0] - self.region_min[0]
-                height = self.region_max[1] - self.region_min[1]
-                if width > 0 and height > 0:
-                    self.out_img = cv2.resize(self.img[y:y+height, x:x+width], self.output_size, self.interpolation).astype(np.uint8)
-                else:
-                    self.out_img = cv2.resize(self.img, self.output_size, self.interpolation).astype(np.uint8)
+            self.check_and_expand_draw_region(pt_2)
+            self.out_img = cv2.resize(self.get_draw_region_img(), self.output_size, self.interpolation).astype(np.uint8)
         else:
             self.out_img = cv2.resize(self.img, self.output_size, self.interpolation).astype(np.uint8)
 
